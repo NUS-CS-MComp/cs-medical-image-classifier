365901b629224cc41a85b970da5c8bae2258e115
Terry Lu <25686506+terryluzj@users.noreply.github.com>
2020-10-13 00:24:51 +0800

    [Common] Changed data loader to use tf.data.Dataset cached/buffered loading

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,92 +1,140 @@
+import os
 import pathlib
-import re
-from typing import Tuple
+from typing import Optional, Tuple
 
-import matplotlib.image as mpimg
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 
 from utils.logger import logger
 
 
 class DataLoader:
-    IMAGE_CACHE_NAME = "train_images.npy"
-    LABEL_CACHE_NAME = "train_labels.npy"
+    """
+    Generic image data loader with custom input workflow embedded in TensorFlow DataSet support for batching loading
+    """
 
     def __init__(
-        self,
-        image_path: pathlib.Path = pathlib.Path(__file__).parent
-        / "../data/train_image/train_image",
-        data_path: pathlib.Path = pathlib.Path(__file__).parent
-        / "../data/train_label.csv",
-        store_path: pathlib.Path = pathlib.Path(__file__).parent / "../data",
+            self,
+            image_path: pathlib.Path = pathlib.Path(__file__).parent / "../data/train_image/train_image",
+            data_path: pathlib.Path = pathlib.Path(__file__).parent / "../data/train_label.csv",
     ):
+        """
+        Initiate data loader
+        :param image_path: Path instance for image path
+        :param data_path: Path instance for data label file path
+        """
+
+        # Initiate path to read image and label files
         self.data_path = data_path
         self.image_path = image_path
-        self.store_path = store_path
 
-        image_cache_path = store_path / DataLoader.IMAGE_CACHE_NAME
-        label_cache_path = store_path / DataLoader.LABEL_CACHE_NAME
-        self.images: np.ndarray = (
-            np.array([])
-            if not image_cache_path.exists()
-            else np.load(str(image_cache_path))
-        )
-        self.labels: np.ndarray = (
-            np.array([])
-            if not label_cache_path.exists()
-            else np.load(str(label_cache_path))
-        )
+        # Initiate labels, classes and training/validation set
+        self.labels: Optional[pd.DataFrame] = None
+        self.classes: Optional[np.ndarray] = None
+        self.image_size: Optional[Tuple[int, int]] = None
 
-    def load_data(self) -> Tuple[np.ndarray, np.ndarray]:
-        def extract_id_from_filename(name: str) -> int:
-            return int(re.findall(r"(\d+).png", name)[0])
+        self.training_ds: Optional[tf.data.Dataset] = None
+        self.validation_ds: Optional[tf.data.Dataset] = None
 
-        if self.images.size != 0 and self.labels.size != 0:
-            logger.info(
-                f"Using cached training data objects at {self.store_path.resolve().absolute()}"
-            )
-            return self.images, self.labels
+    def load(self, batch_size=32, seed=123, image_height=512, image_width=512, validation_split=0.2):
+        """
+        Main function to load dataset object
+        :param batch_size: Batch size
+        :param seed: Seed for randomization
+        :param image_height: Image height
+        :param image_width: Image width
+        :param validation_split: Train/validation split ratio
+        :return: Pre-fetched training and validation TensorFlow dataset object
+        """
 
-        # Load images
-        images = []
-        image_ids = []
-        logger.info(
-            "Reading training data and images and converting them to numpy array"
-        )
+        # Load label data
+        logger.info(f"Reading label .csv data at {self.data_path.resolve()}")
+        labels = pd.read_csv(self.data_path)
+        self.labels = labels.set_index("ID")
+        self.classes = sorted(labels["Label"].unique())  # Important to sort class label in ascending order
+        self.image_size = (image_height, image_width)
 
-        for file in sorted(
-            self.image_path.glob("*.png"),
-            key=lambda x: extract_id_from_filename(x.name),
-        ):
-            image = mpimg.imread(file)
-            image_ids.append(extract_id_from_filename(file.name))
-            images.append(image)
-        self.images = np.array(images)
-        image_ids = np.array(image_ids)
+        # Load image data
+        logger.info(f"Reading image data at {self.image_path.resolve()}")
+        image_ds = tf.data.Dataset.list_files(str(self.image_path / f"*.png"), shuffle=True, seed=seed)
+        image_count = len(image_ds)
 
-        # Load labels
-        label_df = pd.read_csv(self.data_path)
-        ids = label_df.loc[:, "ID"].to_numpy()
-        self.labels = label_df.loc[:, "Label"].to_numpy()
+        # Train-validation data split
+        val_size = int(image_count * validation_split)
+        train_ds = image_ds.skip(val_size)
+        val_ds = image_ds.take(val_size)
 
-        # Consistency check
-        assert np.sum(ids != image_ids) == 0
-        assert ids.shape == image_ids.shape
+        # Map custom process flow
+        logger.info("Running custom input pipelines")
+        train_ds = train_ds.map(self.process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        val_ds = val_ds.map(self.process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-        # Store in disk
-        logger.info(
-            f"Storing training data at {self.store_path.resolve().absolute()}"
-        )
-        np.save(
-            str(self.store_path / DataLoader.IMAGE_CACHE_NAME), self.images
-        )
-        np.save(
-            str(self.store_path / DataLoader.LABEL_CACHE_NAME), self.images
-        )
+        # Add performance configuration on training and validation set
+        logger.info("Tuning dataset batch loading performance")
+        train_ds = DataLoader.configure_for_performance(train_ds, batch_size=batch_size)
+        val_ds = DataLoader.configure_for_performance(val_ds, batch_size=batch_size)
 
-        return self.images, self.labels
+        # Start dataset buffer loading and return prefetched instances
+        self.training_ds = train_ds.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        self.validation_ds = val_ds.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
+        return self.training_ds, self.validation_ds
 
-dl = DataLoader()
-dl.load_data()
+    def process_path(self, file_path: str):
+        """
+        Helper function to process dataset as customized workflow
+        :param file_path: File path in string format
+        :return: Parsed image data and corresponding label given the input file path
+        """
+        label_name = self.get_label(file_path)
+        image_file = tf.io.read_file(file_path)
+        image_data = DataLoader.decode_image(image_file,
+                                             image_height=self.image_size[0],
+                                             image_width=self.image_size[1])
+        return image_data, label_name
+
+    def get_label(self, file_path: str):
+        """
+        Helper function to parse label class
+        :param file_path: File path in string format
+        :return: Single-sized label representation in Tensor form
+        """
+        file_name = tf.strings.split(file_path, os.sep)[-1]
+        file_id = tf.strings.split(file_name, ".")[-2]
+        label_name = tf.gather_nd(self.labels, [tf.strings.to_number(file_id, out_type=tf.dtypes.int32)])
+        return tf.argmax(label_name == self.classes)
+
+    @property
+    def training_data(self):
+        return self.training_ds
+
+    @property
+    def validation_data(self):
+        return self.validation_ds
+
+    @staticmethod
+    def configure_for_performance(dataset: tf.data.Dataset, batch_size: int):
+        """
+        Helper function from https://www.tensorflow.org/tutorials/load_data/images#configure_dataset_for_performance
+        :param dataset: TensorFlow Dataset object
+        :param batch_size: Batch size
+        :return: Performance-wise tuned Dataset object
+        """
+        dataset = dataset.cache()
+        dataset = dataset.shuffle(buffer_size=1000)
+        dataset = dataset.batch(batch_size)
+        dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        return dataset
+
+    @staticmethod
+    def decode_image(image_file: object, image_height: int, image_width: int):
+        """
+        Helper function to decode image file
+        :param image_file: Image buffer file
+        :param image_height: Image height
+        :param image_width: Image width
+        :return: Decoded image file in Tensor form
+        """
+        image_data = tf.image.decode_png(image_file, channels=3)
+        return tf.image.resize(image_data, [image_height, image_width])
